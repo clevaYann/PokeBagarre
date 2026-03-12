84800e2a3d07eeef34af1acdef89101352ac2ac2
jarlincomposer <boussou.jarlin@gmail.com>
2026-03-12 10:47:59 +0100

    Add initial unit tests for Pokemon class functionality

--- src/test/java/com/montaury/pokebagarre/metier/PokemonTest.java
+++ src/test/java/com/montaury/pokebagarre/metier/PokemonTest.java
@@ -1,72 +1,58 @@
 package com.montaury.pokebagarre.metier;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class PokemonTest {
+public class PokemonTest {
 
     @Test
     void test1_premierVainqueurAvecMeilleureAttaque() {
-        // Arrange
         Pokemon p1 = new Pokemon("Pikachu", "url1", new Stats(50, 30));
         Pokemon p2 = new Pokemon("Bulbizarre", "url2", new Stats(40, 30));
 
-        // Act
         boolean resultat = p1.estVainqueurContre(p2);
 
-        // Assert
         assertTrue(resultat, "Le premier devrait gagner car son attaque est supérieure");
     }
 
     @Test
     void test2_secondVainqueurAvecMeilleureAttaque() {
-        // Arrange
         Pokemon p1 = new Pokemon("Pikachu", "url1", new Stats(40, 30));
         Pokemon p2 = new Pokemon("Bulbizarre", "url2", new Stats(50, 30));
 
-        // Act
         boolean resultat = p1.estVainqueurContre(p2);
 
-        // Assert
         assertFalse(resultat, "Le second devrait gagner car son attaque est supérieure");
     }
 
     @Test
     void test3_premierVainqueurAvecAttaqueEtDefenseEgale() {
-        // Arrange
         Pokemon p1 = new Pokemon("Pikachu", "url1", new Stats(50, 50));
         Pokemon p2 = new Pokemon("Bulbizarre", "url2", new Stats(50, 50));
 
-        // Act
         boolean resultat = p1.estVainqueurContre(p2);
 
-        // Assert
         assertTrue(resultat, "Le premier devrait gagner en cas d'égalité parfaite");
     }
 
     @Test
     void test4_premierVainqueurAvecAttaqueEgaleEtMeilleureDefense() {
-        // Arrange
         Pokemon p1 = new Pokemon("Pikachu", "url1", new Stats(50, 60));
         Pokemon p2 = new Pokemon("Bulbizarre", "url2", new Stats(50, 40));
 
-        // Act
         boolean resultat = p1.estVainqueurContre(p2);
 
-        // Assert
         assertTrue(resultat, "Le premier devrait gagner avec une meilleure défense à attaque égale");
     }
 
     @Test
     void test5_secondVainqueurAvecAttaqueEgaleEtMeilleureDefense() {
-        // Arrange
         Pokemon p1 = new Pokemon("Pikachu", "url1", new Stats(50, 40));
         Pokemon p2 = new Pokemon("Bulbizarre", "url2", new Stats(50, 60));
 
-        // Act
         boolean resultat = p1.estVainqueurContre(p2);
 
-        // Assert
         assertFalse(resultat, "Le second devrait gagner avec une meilleure défense à attaque égale");
     }
 }
